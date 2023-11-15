87faa68ccf51fe1daf18615a48d90b330330bb4a
Rakshith Shivaramappa Pushpa <11018585@STUD.HOCHSCHULE-HEIDELBERG.DE>
2023-11-15 05:52:52 +0000

    Heat Wave Transformation Code

--- models/RSP/UniqueDisasters.sql
+++
@@ -1,8 +0,0 @@
-WITH UniqueDisasters AS (
-  SELECT
-    DISTINCT DC.string_field_3 AS UniqueDisasters
-  FROM
-    `rsp-data-engineering-ii.eu_disaster.DisasterClassification` AS DC
-)
-
-SELECT * FROM UniqueDisasters
