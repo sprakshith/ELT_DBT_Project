723b8b4c7f891c4bb0aae49e3a3606819b01bb5f
Shreya Kalkeri <shreya.kalkeri@stud.hochschule-heidelberg.de>
2023-11-15 08:48:58 +0000

    Commit Data models

---
+++ models/SRK/Dim_Quality.sql
@@ -0,0 +1,17 @@
+SELECT
+    earthquake_id,  
+    felt,
+    cdi,
+    mmi,
+    alert,
+    status,
+    sig,
+    net,
+    nst,
+    dmin,
+    rms,
+    gap,
+    magType,
+    geometryType,
+    depth
+FROM `rsp-data-engineering-ii.eu_disaster.Earthquake`
